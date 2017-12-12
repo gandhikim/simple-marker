62b3d289256dc14c3823037a29467654532495de
tenbloodman <tenbloodman@gmail.com>
2017-12-12 17:14:28 +0900

    delete marker

--- simple-marker/src/jquery.sp-marker.js
+++ simple-marker/src/jquery.sp-marker.js
@@ -5,7 +5,7 @@
         var _this = this;
 
 		var defaults = {
-            color : "#FFFF00",
+            color : "#FFFF00"
         };
 
 		options = $.extend({}, defaults, options);
@@ -14,25 +14,37 @@
             document.designMode = "on";
         };
 
-        var this_mouseup = function() {
-            var sel = window.getSelection();
-            var range = sel.getRangeAt(0);
+        var this_mouseup = function(e) {
 
-            if (sel && range) {
-                sel.removeAllRanges();
-                sel.addRange(range);
+            var _target = e.target;
+            var _which = e.which;
+
+            if(_which === 1) {
+                var sel = window.getSelection();
+                var range = sel.getRangeAt(0);
+
+                if (sel && range) {
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+
+                var rangeCnt = range.toString().length;
+                if(rangeCnt > 3) {
+                    document.execCommand("backcolor", false, options.color);
+                }
+
+                if(sel) {
+                    sel.removeAllRanges();
+                }
+
+                document.designMode = "off";
+
+            } else if(_which === 3) {
+                _this.text(
+                    _this.html().replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "")
+                );
             }
 
-            var rangeCnt = range.toString().length;
-            if(rangeCnt > 3) {
-                document.execCommand("backcolor", false, options.color);
-            }
-         
-            if(sel) {
-                sel.removeAllRanges();
-            }
-
-            document.designMode = "off";
         };
 
         _this.on("mousedown", this_mousedown)
@@ -40,4 +52,3 @@
             
     };
 }(jQuery));
-
